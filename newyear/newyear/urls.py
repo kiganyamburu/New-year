569bb8fd6615b1f145d2a505d625afdab696c2f9
kiganyamburu <mburukiganya@gmail.com>
2024-11-07 02:29:08 +0300

    debugging 101

--- newyear/newyear/urls.py
+++ newyear/newyear/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for newyear project.
+URL configuration for new project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -12,11 +12,14 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
+    
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from new import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("new.urls")),
 ]
