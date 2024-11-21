9b7c15017df80f494341ad1abfd6f1a3867cc25e
kiganyamburu <mburukiganya@gmail.com>
2024-11-21 03:06:09 +0300

    removed

--- newyear/newyear/urls.py
+++ newyear/newyear/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from new import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('new/', include("new.urls")),
+    path('', views.index,name='index'),
 ]
